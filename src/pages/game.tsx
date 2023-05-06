9d2339bd8a5ee11f4df346ee16493fd6f8f8198c
Tsvetan <tzpet9@gmail.com>
2023-05-06 16:16:04 +0300

    Added scoring function for a single cell

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -38,8 +38,8 @@ function Game() {
 
   function place(board: number[][], col: number) {
     for (let i = 9; i >= 0; i--) {
-      if (board?.[i]?.[col] === 0) {
-        board![i]![col] = currentTurn;
+      if (board[i]![col] === 0) {
+        board[i]![col] = currentTurn;
         return true;
       }
     }
@@ -67,6 +67,51 @@ function Game() {
     return (
       score == -finalScore || score == finalScore || depth == 0 || isFull(board)
     );
+  }
+
+  function scorePosition(
+    board: number[][],
+    row: number,
+    column: number,
+    delta_y: number,
+    delta_x: number
+  ) {
+    //recieves a board array and positon to score
+    let human_points = 0;
+    let computer_points = 0;
+
+    // Save winning positions to arrays for later usage
+    let winning_array_human = [];
+    let winning_array_cpu = [];
+
+    // Determine score through amount of available chips
+    for (var i = 0; i < 4; i++) {
+      if (board[row]![column] == 0) {
+        winning_array_human.push([row, column]);
+        human_points++; // Add for each human chip
+      } else if (board[row]![column] == 1) {
+        winning_array_cpu.push([row, column]);
+        computer_points++; // Add for each computer chip
+      }
+
+      // Moving through our board
+      row += delta_y;
+      column += delta_x;
+    }
+
+    // Marking winning/returning score
+    if (human_points == 4) {
+      winningArray = winning_array_human;
+      // Computer won (100000)
+      return -finalScore;
+    } else if (computer_points == 4) {
+      winningArray = winning_array_cpu;
+      // Human won (-100000)
+      return finalScore;
+    } else {
+      // Return normal points
+      return computer_points;
+    }
   }
 
   return (
